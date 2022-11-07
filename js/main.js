2162cdfa3e39c651db6e614a404793b2cc51cce0
Schettino <edu.schettino98@gmail.com>
2022-11-06 21:22:32 -0300

    se finaliza archivo js para segunda entrega de CoderHouse

--- js/main.js
+++ js/main.js
@@ -1,66 +1,3 @@
-//                                        PRIMERA ENTREGA
-
-
-// function darBienvenida() {
-//     alert("Bienvenido " + nombre + ", ¿Que pelicula deseas ver?")
-//     console.log("Bienvenido " + nombre + ", ¿Que pelicula deseas ver?")
-//     console.log("Selecciona un numero, dependiendo de la pelicula que deseas ver: ")
-//     console.log("1. Avengers")
-//     console.log("2. Hulk")
-//     console.log("3. Thor")
-//     console.log("4. Spiderman")
-// }
-
-// function entradasDis() {
-//     console.log("Actualmente se encuentran disponible " + entradasDisponibles + " Entradas")
-//     alert("Actualmente se encuentran disponible " + entradasDisponibles + " Entradas")
-// }
-
-// function elegirEntradas(seleccionPelicula) {
-//     let entradas = Number(prompt("¿Cuantas entradas deseas comprar para " + seleccionPelicula + "?"))
-//     if (entradas > entradasDisponibles) {
-//         alert("Entradas no disponibles!")
-//     }
-//     else {
-//         entradasDisponibles = entradasDisponibles - entradas
-//         alert("Redirigiendo a la pagina para pagar... magicamente")
-//     }
-
-// }
-
-// entradasDisponibles = 10
-
-// let nombre = prompt("Ingresa tu nombre completo: ")
-// darBienvenida(nombre)
-// if (entradasDisponibles > 1) {
-//     while (entradasDisponibles > 0) {
-//         entradasDis()
-//         seleccionPelicula = prompt("Selecciona un numero, dependiendo de la pelicula que deseas ver: \n1.Avengers \n2.Hulk \n3.Thor \n4.Spiderman ")
-//         seleccionPelicula = parseInt(seleccionPelicula)
-//         switch (seleccionPelicula) {
-//             case 1:
-//                 seleccionPelicula = "Avengers"
-//                 elegirEntradas(seleccionPelicula)
-//                 break;
-//             case 2:
-//                 seleccionPelicula = "Hulk"
-//                 elegirEntradas(seleccionPelicula)
-//                 break;
-//             case 3:
-//                 seleccionPelicula = "Thor"
-//                 elegirEntradas(seleccionPelicula)
-//                 break;
-//             case 4:
-//                 seleccionPelicula = "Spiderman"
-//                 elegirEntradas(seleccionPelicula)
-//                 break;
-//             default:
-//                 alert("No seleccionaste ninguna pelicula!")
-//                 break;
-//         }
-//     }
-//     alert("Ya no quedan entradas!")
-// }
 
 
 //                                       SEGUNDA ENTREGA 
@@ -78,9 +15,10 @@ class pelicula {
 //USUARIOS GUARDADO Y USUARIO ADMIN (PARA PRUEBA DE INICIO DE SESION)
 const usuariosCreados = []
 const admin = [usuario = "eduardo", correo = "edu.schettino98@gmail.com", contraseña = "123abc"]
-
+const valorEntrada = 3900
 //PELICULAS
 const peliculas = []
+const carrito = []
 //ESTRENOS
 peliculas.push(new pelicula("Avengers", 20, Boolean(true), "12:30"))
 peliculas.push(new pelicula("Hulk", 20, Boolean(true), "14:00"))
@@ -91,7 +29,6 @@ peliculas.push(new pelicula("Amsterdam", 20, Boolean(false), "10:00"))
 peliculas.push(new pelicula("El Cuarto Pasajero", 20, Boolean(false), "9:00"))
 peliculas.push(new pelicula("REC", 20, Boolean(false), "16:20"))
 peliculas.push(new pelicula("Al Oriente", 20, Boolean(false), "7:00"))
-console.log(peliculas)
 
 
 function registro() {
@@ -104,9 +41,8 @@ function registro() {
     else {
         arrayPush = { nombre: usuario, correo: correo, contraseña: contraseña }
         usuariosCreados.push(arrayPush)
-        console.log(usuariosCreados)
         alert("Bienvenido " + usuario + ", Tu cuenta ha sido creada con Exito")
-        alert("Por favor inicie secion.")
+        alert("Por favor inicie sesion.")
         iniciarSesion()
     }
 }
@@ -118,78 +54,147 @@ function iniciarSesion() {
     if (busqueda !== undefined && busqueda.nombre == usuario && busqueda.contraseña == contraseña) {
         alert("Inicio de sesion Exitoso!")
         estado = true
+        tipodePeli()
     }
     else {
         alert("Datos Incorrectos!")
-        iniciarSesion()
     }
-    console.log(busqueda)
+}
+function comprar(eleccionPeli, buscarPelicula) {
+    eleccionPeli = eleccionPeli - 1
+    while (true) {
+        let cantidad = Number(prompt("Ingresa la cantidad de entradas que deseas de la pelicula " + buscarPelicula[eleccionPeli].pelicula + ":"))
+        if (cantidad <= buscarPelicula[eleccionPeli].entradasDisponibles && cantidad >= 0 && cantidad !== 0) {
+            buscarPelicula[eleccionPeli].entradasDisponibles = buscarPelicula[eleccionPeli].entradasDisponibles - cantidad
+            let compra = cantidad * valorEntrada
+            alert("La entrada tiene un valor de $" + valorEntrada + " CLP, por lo tanto tu compra sera de: $" + compra + ", sera agregada a tu carrito.")
+            buscarPelicula[eleccionPeli].valor = compra
+            buscarPelicula[eleccionPeli].entradasToBuy = cantidad
+
+            carrito.push(buscarPelicula[eleccionPeli])
+            finalizando()
+
+            break
+        }
+        alert("Se excede del limite de entradas!, vuele a intentarlo por favor.")
+        cantidad = 0
+    }
 }
 
 //BUSCA LAS PELICULAS SEGUN EL FILTRO (ESTRENO O NO)
-function buscarPeli(filtro){
-    
-
-    
-
-
-}
-
-
-
-
-
-
-
-let estado = false
-while (estado == false) {
-    let opcionBienvenida = Number(prompt("Bienvenido al sitio web del Cine \n ¿Deseas (1) registrarte, (2) iniciar sesion o seguir como (3) invitado?"))
-    if (opcionBienvenida == 1 || opcionBienvenida == 2 || opcionBienvenida == 3) {
-        switch (opcionBienvenida) {
-            case 1:
-                registro()
-                break;
-            case 2:
-                iniciarSesion()
-                break;
-            case 3:
-                estado = true
-                break;
+function buscarPeli(filtro) {
+    const buscarPelicula = peliculas.filter((i) => i.estreno == filtro) //  BUSCA LAS PELICULAS QUE CUMPLAN CON EL FILTRO DE : ES ESTRENO O NO?
+    while (true) {
+        const eleccionPeli = Number(prompt("¿Para que pelicula deseas comprar entradas?\n" + "(1) " + buscarPelicula[0].pelicula + "  ----  Entradas disponibles: " + buscarPelicula[0].entradasDisponibles + "\n (2) " +
+            buscarPelicula[1].pelicula + "  ----  Entradas disponibles: " + buscarPelicula[1].entradasDisponibles + "\n (3) " + buscarPelicula[2].pelicula + "  ----  Entradas disponibles: " + buscarPelicula[2].entradasDisponibles + "\n (4) " + buscarPelicula[3].pelicula + "  ----  Entradas disponibles: " + buscarPelicula[3].entradasDisponibles))
+        if (eleccionPeli == 1 || eleccionPeli == 2 || eleccionPeli == 3 || eleccionPeli == 4) {
+            comprar(eleccionPeli, buscarPelicula)
+            break
+        }
+        else {
+            alert("Esa opcion no existe")
         }
     }
-    else {
-        alert("Esa opcion no existe")
+
+
+
+
+}
+
+function primeringreso() {
+    let estado = false
+    while (estado == false) {
+        let opcionBienvenida = Number(prompt("Bienvenido al sitio web del Cine \n ¿Deseas (1) Registrarte, (2) iniciar sesion o seguir como (3) invitado?"))
+        if (opcionBienvenida == 1 || opcionBienvenida == 2 || opcionBienvenida == 3) {
+            switch (opcionBienvenida) {
+                case 1:
+                    registro()
+                    break;
+                case 2:
+                    iniciarSesion()
+                    break;
+                case 3:
+                    estado = true
+                    tipodePeli()
+                    break;
+            }
+        }
+        else {
+            alert("Esa opcion no existe")
+        }
     }
 }
+
 
 //FILTRO ENTRE ESTRENO O PELICULAS NORMALES
-let estado2 = false
-while (estado2 == false) {
-    const filtro = Number(prompt("¿Que tipo de pelicula deseas ver? \n (1)Estrenos \n (2)Normales"))
-    if (filtro == 1 || filtro == 2) {
-        switch (filtro) {
-            case 1:
-                console.log("opcion 1")
-                filtro == 
-                buscarPeli(filtro)
 
-                estado2 = true
-                break;
-            case 1:
-                console.log("opcion 2")
-                estado2 = true
-                break;
+function tipodePeli() {
+    let estado2 = false
+    while (estado2 == false) {
+        let filtro = Number(prompt("¿Que tipo de pelicula deseas ver? \n (1)Estrenos \n (2)Normales"))
+        if (filtro == 1 || filtro == 2) {
+            switch (filtro) {
+                case 1:
+                    filtro = Boolean(true)
+                    buscarPeli(filtro)
+                    estado2 = true
+                    break;
+                case 2:
+                    filtro = Boolean(false)
+                    buscarPeli(filtro)
+                    estado2 = true
+                    break;
 
 
+            }
+        }
+        else {
+            alert("Esa opcion no existe")
         }
     }
-    else {
-        alert("Esa opcion no existe")
-    }
-
-
 }
 
+function finalizando() {
 
 
+    while (true) {
+        let finalizar = Number(prompt("¿Deseas (1) Finalizar tu compra, (2) Ver tu carrito o (3) Comprar mas entradas?"))
+        if (finalizar == 1 || finalizar == 2 || finalizar == 3) {
+            switch (finalizar) {
+                case 1:
+                    let total = 0
+                    carrito.forEach(function (a) { total += a.valor })
+                    console.log(total)
 
+                    alert("Seras redirigido a la pagina para pagar el total de tu compra, el cual es de: $" + total)
+
+                    break;
+                case 2:
+                    productos = []
+                    carrito.forEach(element => {
+                        productos.push(element.pelicula + " -- " + element.entradasToBuy + " Entradas -- $ " + element.valor)
+                        console.log(productos)
+                    })
+                    const mensaje = productos.join("\n")
+                    console.log(mensaje)
+                    alert("Las entradas que compraras son las siguientes: \n" + mensaje)
+                    finalizando()
+
+                    break;
+                case 3:
+                    tipodePeli()
+
+                    break;
+
+            }
+            break
+
+        }
+        else {
+            alert("Esa opcion no existe")
+        }
+    }
+}
+for (let i = 0; i < 1; i++) {
+    primeringreso()
+}
