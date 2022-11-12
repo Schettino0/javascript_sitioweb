73c01420b1f2acacaa19f56c863cd8f3b0d98a76
Schettino <edu.schettino98@gmail.com>
2022-11-12 02:31:24 -0300

    se agregan horarios en visual, y se prueba objetos dentro de objetos

--- js/main.js
+++ js/main.js
@@ -15,11 +15,34 @@ const pelicula5 = new pelicula("5", "Amsterdam", 20, "../assest/img/amsterdam.we
 const pelicula6 = new pelicula("6", "El Cuarto Pasajero", 20, "../assest/img/elcuartopasajero.jpg", 0)
 const pelicula7 = new pelicula("7", "REC", 20, "../assest/img/red.jpg", 0)
 const pelicula8 = new pelicula("8", "Al Oriente", 20, "../assest/img/aloriente.jpg", 0)
-
 const peliculas = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7, pelicula8]
 
 
-const containerEstrenos = document.querySelector(".container__carteles")
+//EJEMPLO DE OBJETO CON OBJETO DENTRO , DISTINTAS ENTRADAS DEPENDIENDO DEL HORARIO DE LA FUNCION.
+const peliEjemplo = {
+    tituloPelicula: "Avengers",
+    horarios: {
+        funcion1: {
+            hora: "12:30",
+            entradas: 20
+        },
+        funcion2: {
+            hora: "14:00",
+            entradas: 30
+        },
+        funcion3: {
+            hora: "16:00",
+            entradas: 30
+        }
+    },
+    entreno: 1
+}
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+const containerEstrenos = document.querySelector(".container__estrenos")
 const containerPeliculas = document.querySelector(".container__peliculas")
 
 const renderizar = () => {
@@ -28,7 +51,14 @@ const renderizar = () => {
         tarjeta.className = "container__carteles--box"
         tarjeta.innerHTML = `<img src="${producto.imgsrc}"/>
         <h3>${producto.nombre}</h3>
-        <button data-id= ${producto.id}>Comprar Entradas</button>`
+        <button  id="verhorarios" data-id= ${producto.id}>Ver Horarios</button>
+        <div class="horarios">
+        <h4>Funciones: </h4>
+        <button class="buttonhora">12:30</button><button class="buttonhora">14:00</button><button
+        class="buttonhora">16:00</button><br>   
+        <button
+        class="buttonhora">COMPRAR</button>
+        </div>`
         if (producto.estreno == 1) {
             containerEstrenos.append(tarjeta)
         }
@@ -38,12 +68,23 @@ const renderizar = () => {
         }
     })
 }
+const mostrarhorarios = () => {
+    const btnVerhorarios = document.querySelectorAll("#verhorarios")
+    const divHorarios = document.querySelectorAll(".horarios")
+    btnVerhorarios.forEach((e)=>{
+        e.addEventListener("click",(i)=>{
+            const bloque= e.parentElement
+            const editar = bloque.querySelector(".horarios")
+            editar.style.display= "block"   
 
+        })
+    })
+
+}
 
 
 renderizar()
-prueba()
-
+mostrarhorarios()
 
 
 
