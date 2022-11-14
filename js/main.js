04c9ff5fb8b38131272bcb097b2d430b52d5c857
Schettino <edu.schettino98@gmail.com>
2022-11-14 17:01:17 -0300

    se añade carrito y logica para restar entradas

--- js/main.js
+++ js/main.js
@@ -13,14 +13,26 @@ class funcionesCine {
         this.entradas = entradas
     }
 }
+
+class addCarrito {
+    constructor(titulo, entradas, funcion) {
+        this.titulo = titulo
+        this.entradas = entradas
+        this.funcion = funcion
+    }
+}
+
+
+
 //FUNCIONES AM 
-const funcionAM1 = new funcionesCine("8:30", 20)
-const funcionAM2 = new funcionesCine("10:30", 20)
-const funcionAM3 = new funcionesCine("11:00", 20)
+const funcionAM1 = new funcionesCine("8:30", 30)
+const funcionAM2 = new funcionesCine("10:30", 30)
+const funcionAM3 = new funcionesCine("11:00", 30)
 //FUNCIONES PM
-const funcionPM1 = new funcionesCine("12:30", 20)
-const funcionPM2 = new funcionesCine("17:30", 20)
-const funcionPM3 = new funcionesCine("22:30", 20)
+const funcionPM1 = new funcionesCine("12:30", 30)
+const funcionPM2 = new funcionesCine("17:30", 30)
+const funcionPM3 = new funcionesCine("22:30", 30)
+
 
 const funcionesAM = [funcionAM1, funcionAM2, funcionAM3]
 const funcionesPM = [funcionPM1, funcionPM2, funcionPM3]
@@ -33,9 +45,17 @@ const pelicula5 = new pelicula("5", "Amsterdam", "../assest/img/amsterdam.webp",
 const pelicula6 = new pelicula("6", "El Cuarto Pasajero", "../assest/img/elcuartopasajero.jpg", 0, funcionesPM)
 const pelicula7 = new pelicula("7", "REC", "../assest/img/red.jpg", 0, funcionesAM)
 const pelicula8 = new pelicula("8", "Al Oriente", "../assest/img/aloriente.jpg", 0, funcionesPM)
-const peliculas = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7, pelicula8]
+let peliculas = [pelicula1, pelicula2, pelicula3, pelicula4, pelicula5, pelicula6, pelicula7, pelicula8]
 
-const carrito = []
+let carrito = []
+
+
+
+
+
+//LOCALSTORAGE
+
+
 
 //EJEMPLO DE OBJETO CON OBJETO DENTRO , DISTINTAS ENTRADAS DEPENDIENDO DEL HORARIO DE LA FUNCION.
 const peliEjemplo = {
@@ -67,17 +87,20 @@ const renderizar = () => {
         tarjeta.className = "container__carteles--box"
         tarjeta.innerHTML = `<img src="${producto.imgsrc}"/>
         <h3>${producto.nombre}</h3>
-        <button  id="verhorarios" data-id= ${producto.id}>Ver Horarios</button>
+        <button  id="verhorarios" >Ver Horarios</button>
         <div class="horarios">
         <h4>Funciones: </h4>
         <div class="opciones">
-        <form class="formulario" id="${producto.nombre}">
+        <form class="formulario" data-id= "${producto.id}" id ="${producto.nombre}">
         <input type="radio" name="horario" id="${i}" value=${producto.horarios[0].hora}>
         <label for=${i}>${producto.horarios[0].hora}</label>
         <input type="radio" name="horario" id="${i + 1}" value=${producto.horarios[1].hora}>
         <label for="${i + 1}">${producto.horarios[1].hora}</label>
         <input type="radio" name="horario" id="${i + 2}" value=${producto.horarios[2].hora}>
         <label for="${i + 2}">${producto.horarios[2].hora}</label><br>
+        <div class="entradasDisp">
+        <span>${producto.horarios[0].entradas}──</span><span id="mostrar">${producto.horarios[1].entradas}──</span><span id="mostrar">${producto.horarios[2].entradas}</span>
+        </div>
         <!--SELECIONAR CANTIDAD DE ENTRADAS -->
         <h4>Cantidad:</h4>
         <select name="cantidad">
@@ -86,7 +109,8 @@ const renderizar = () => {
         <option value=3>3</option>
         <option value=4>4</option>
         </select> <br>
-        <input class="buttonhora" type="submit" value="Agregar al Carrito">
+        <input class="buttonhora" type="submit" value="Agregar al Carrito" id="carrito">
+        <input class="buttonhora" type="reset" value="Limpiar">
         </form>
         </div>`
         if (producto.estreno == 1) {
@@ -121,30 +145,82 @@ const mostrarhorarios = () => {
 
 const formulario = () => {
 
-    const opciones = document.querySelectorAll(".opciones")
-    const boton = document.querySelector(".buttonhora")
     const formulario = document.querySelectorAll(".formulario")
     formulario.forEach((e) => {
         e.addEventListener('submit', (x) => {
-            console.log(e)
-            let transactionFormData = new FormData(e)
-            let cantidad = transactionFormData.get("cantidad")
-            let funcion = transactionFormData.get("horario")
+            x.preventDefault()
+            const transactionFormData = new FormData(e)
+            const cantidad = transactionFormData.get("cantidad")
+            const funcion = transactionFormData.get("horario")
             const movie = e.getAttribute('id')
+            console.log(movie)
+            const id = e.getAttribute("data-id")
             const mensaje = `${cantidad} Entradas para la funcion de las ${funcion} para ${movie}`
             console.log(mensaje)
-            x.preventDefault()
-            agregarCarrito(cantidad,funcion,movie)
+            if (funcion == null) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Debes seleccionar una funcion!'
+                })
+            }
+            else {
+                agregarCarrito(cantidad, funcion, id, movie)
+            }
         })
     })
 
 }
 
-const agregarCarrito= (cantidad,funcion,movie)=>{
-    console.log("hola" + cantidad + funcion +movie)
+const agregarCarrito = (cantidad, funcion, id, movie) => {
+    const eleccion = peliculas.find((i) => i.nombre == movie)
+    const add = new addCarrito(eleccion.nombre, cantidad, funcion)
+    let resta = eleccion.horarios.find(a => a.hora == funcion)
+    resta.entradas -= cantidad
+    carrito.push(add)
+    const carroJSON = JSON.stringify(carrito)
+    localStorage.setItem("carrito", carroJSON)
+    const peliculasJSON = JSON.stringify(peliculas)
+    localStorage.setItem("peliculas", peliculasJSON)
+
 }
 
+const cargarCarrito = () => {
+    if (localStorage.getItem("carrito") !== null) {
+        // Carga la información
+        carrito = JSON.parse(localStorage.getItem('carrito'));
+    }
+}
+
+const cargarPeliculas = () => {
+    if (localStorage.getItem("peliculas") !== null) {
+        // Carga la información
+        peliculas = JSON.parse(localStorage.getItem('peliculas'));
+    }
+    else {
+        const peliculasJSON = JSON.stringify(peliculas)
+        localStorage.setItem("peliculas", peliculasJSON)
+        location.reload()
+    }
+}
+
+
+// const actualizarEntradas = () => {
+//     peliculas.forEach((producto) => {
+//             const ubicacion = document.querySelector(".entradasDisp")
+//             const div = document.createElement("div")
+//             div.className = "a"
+//             div.innerHTML = `<span>${producto.horarios[0].entradas}──</span><span id="mostrar">${producto.horarios[1].entradas}──</span><span id="mostrar">${producto.horarios[2].entradas}</span>`
+//             ubicacion.append(div)
+
+//         })
+// }
+
+// const info = `<span>${producto.horarios[0].entradas}──</span><span id="mostrar">${producto.horarios[1].entradas}──</span><span id="mostrar">${producto.horarios[2].entradas}</span>`
+
+cargarPeliculas()
 renderizar()
+cargarCarrito()
 mostrarhorarios()
 formulario()
 
